afc6f821abeb8b522fab4ef6b5229805c8ee89b6
Sera <garanina_valeriya@mail.ru>
2025-07-26 23:03:57 +0500

    task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b**2 - 4 * a * c;
+
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    arr = [-b / (2 * a)];
+  } else if (d > 0) {
+    arr = [(-b + Math.sqrt(d) ) / (2 * a) , (-b - Math.sqrt(d) ) / (2 * a)];
+  }
   
   return arr;
 }
+console.log(solveEquation());
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let p = (percent / 100) / 12;
+  let s = amount - contribution;
+  let n = countMonths;
+  let payInMonth = s * (p + (p / (((1 + p) ** n) - 1))); 
+  let sum = +(payInMonth * countMonths).toFixed(2);
+
+  return sum;
 }
+console.log(calculateTotalMortgage());
