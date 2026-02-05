7a68c63be646ba527c599658bda3dc52d2d566d3
SeraV <garanina_valeriya@mail.ru>
2026-02-05 15:31:11 +0500

    task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.subject.hasOwnProperty('marks')) {
+        this.subject.marks.push(...marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.subject.hasOwnProperty('marks') && 
+      this.marks.length === 0) {
+        return 0;
+    } 
+
+    let sum = marks.reduce((acc, curr) => acc + curr, 0); 
+    this.subject.getAverage = sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.marks;
+    delete this.subject;
+    this.excluded = reason;
 }
