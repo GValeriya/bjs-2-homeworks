a02440aa45ed259d7499e76b768710c812bb6595
Sera <garanina_valeriya@mail.ru>
2026-04-02 17:41:49 +0500

    task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -5,28 +5,30 @@ function Student(name, gender, age) {
   this.marks = [];
 }
 
+let student = new Student();
+
 Student.prototype.setSubject = function (subjectName) {
     this.subject = subjectName;
-}
+} 
 
-Student.prototype.addMarks = function (...marks) {
-    if (this.subject.hasOwnProperty('marks')) {
-        this.subject.marks.push(...marks);
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (student.hasOwnProperty('marks')){
+        this.marks.push(...marksToAdd)
     }
 }
 
 Student.prototype.getAverage = function () {
-    if (!this.subject.hasOwnProperty('marks') && 
+    if (!student.hasOwnProperty('marks') || 
       this.marks.length === 0) {
         return 0;
     } 
 
-    let sum = marks.reduce((acc, curr) => acc + curr, 0); 
-    this.subject.getAverage = sum / this.marks.length;
+    let sum = this.marks.reduce((acc, curr) => acc + curr, 0); 
+    return  sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
+    this.excluded = reason;
     delete this.marks;
     delete this.subject;
-    this.excluded = reason;
 }
